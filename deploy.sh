356b5a70c23ccf1035c3a279c8a47d78bd9b40e7
Florent <florent_465@hotmail.fr>
2022-04-16 17:32:29 +0200

    fix typo on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile .server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile .worker
+docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push stephengrider/multi-client:latest
 docker push stephengrider/multi-server:latest
 docker push stephengrider/multi-worker:latest
