7ba3fdcfda62a3016012f8f5df23da2e52b62e8b
Florent <florent_465@hotmail.fr>
2022-04-16 18:34:01 +0200

    change to florentb1 images

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t florentb1/multi-client:latest -t florentb1/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t florentb1/multi-server:latest -t florentb1/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t florentb1/multi-worker:latest -t florentb1/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push florentb1/multi-client:latest
+docker push florentb1/multi-server:latest
+docker push florentb1/multi-worker:latest
 
-docker push stephengrider/multi-client:latest:$SHA
-docker push stephengrider/multi-server:latest:$SHA
-docker push stephengrider/multi-worker:latest:$SHA
+docker push florentb1/multi-client:latest:$SHA
+docker push florentb1/multi-server:latest:$SHA
+docker push florentb1/multi-worker:latest:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=florentb1/multi-server:$SHA
+kubectl set image deployments/client-deployment client=florentb1/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=florentb1/multi-worker:$SHA
